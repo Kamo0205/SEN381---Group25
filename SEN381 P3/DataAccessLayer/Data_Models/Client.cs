50e451499c934df8cebaae1b884fdf9106a06519
JohanSch_1412 <johanschsw@gmail.com>
2021-05-10 22:08:39 +0200

    Huge refactoring, merging 2nd repo into initial repo???

---
+++ SEN381 P3/DataAccessLayer/Data_Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    public abstract class Client : Person
+    {
+        private string location;
+        private Contract contract;
+
+        public string Location { get => location; set => location = value; }
+        public Contract Contract { get => contract; set => contract = value; }
+
+        public Client(string firstName, string lastName, string phoneNumber, string location) : base(firstName, lastName, phoneNumber) => this.location = location;
+    }
+}
---
+++ SEN381 P3/PresentationLayer/ServiceDepartment.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class FrmServiceDepartment : Form
+    {
+        public FrmServiceDepartment()
+        {
+            InitializeComponent();
+            setContext();
+            setData();
+        }
+
+        public void setData()
+        {
+            Dictionary<string, string> details = new Dictionary<string, string>();
+            if (details.Count > 0)
+            {
+                txtSkill.Text = details["skill"];
+                txtClassfication.Text = details["classification"];
+                txtEmployeeName.Text = details["firstName"];
+            }
+           
+        }
+
+        public void setContext()
+        {
+            BindingSource bsSource = new BindingSource();
+            lstTechnicians.DataSource = bsSource;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            FrmWelcomeScreen welcomeScreen = new FrmWelcomeScreen();
+            this.Hide();
+            welcomeScreen.ShowDialog();
+            this.Close();
+        }
+
+        private void lstTechnicians_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setData();
+        }
+
+        private void btnLockInTechnicians_Click(object sender, EventArgs e)
+        {
+            lstTechnicians.Enabled = !lstTechnicians.Enabled;
+            txtEmployeeName.Enabled = !txtEmployeeName.Enabled;
+            txtSkill.Enabled = !txtSkill.Enabled;
+            txtClassfication.Enabled = !txtClassfication.Enabled;
+        }
+
+        private void btnLockInEquipment_Click(object sender, EventArgs e)
+        {
+            txtEquipment.Enabled = !txtEquipment.Enabled;
+        }
+    }
+}
